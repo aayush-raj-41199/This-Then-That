41c1072d3872ba66b56eab73ad8af64a3ecb34a3
aayush-raj-41199 <aayushraj4111999@gmail.com>
2022-08-28 19:03:48 +0530

    Start Screen Complete

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject ground1;
     public GameObject ground2;
+    public Button tapToPlay;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +25,6 @@ public class MainMenuScript : MonoBehaviour
     {
         ground1.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
         ground2.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        tapToPlay.gameObject.SetActive(false);
     }
 }
